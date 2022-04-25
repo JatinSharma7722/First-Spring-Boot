1a3b8f215e1414bdef3fb34d8fea76d918dff541
JatinSharma7722 <jatinsharmakatian.cse19@chitkarauniversity.edu.in>
2022-04-25 15:48:10 +0530

    initial commit

---
+++ firstSpringBootApp1/src/main/java/springBootApp/examples/firstSpringBootApp1/FirstSpringBootApp1Application.java
@@ -0,0 +1,13 @@
+package springBootApp.examples.firstSpringBootApp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringBootApp1Application.class, args);
+	}
+
+}
---
+++ firstSpringBootApp1/src/test/java/springBootApp/examples/firstSpringBootApp1/FirstSpringBootApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package springBootApp.examples.firstSpringBootApp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
